f15dbabd5dd35e1c2338204d8efa93ad4431d83c
Max <maxim-dollor@yandex.by>
2017-11-15 21:00:56 +0300

    EL - [0.2] - Add and Configure log4net for EventsListWebApp

--- EventsListWebApp/Controllers/HomeController.cs
+++ EventsListWebApp/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace EventsListWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ILog log = LogManager.GetLogger("HomeController");
+
         // GET: Home
         public ActionResult Index()
         {
+            log.Debug("Test Log");
             return View();
         }
     }
