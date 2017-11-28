23b0984eb789d2f9a5d662c4f98d146f0298110a
axe607 <maxim-dollor@yandex.by>
2017-11-28 14:29:00 +0300

    EL - [0.6] - Update Categories load script; Change Events block styles

--- EventsListWebApp/Scripts/categoriesbar.js
+++ EventsListWebApp/Scripts/categoriesbar.js
@@ -1,5 +1,13 @@
 ﻿function loadCategoriesBar() {
-    $("#categories").load("Category/CategoriesBar");
+    $("#categories").load("Category/CategoriesBar", function(response, status, xhr) {
+        if (status === "error")
+        {
+            console.log("[CategoryBar]: "+xhr.status + " - " + xhr.statusText);
+            var errorText = document.createElement("b");
+            errorText.innerText = "Some errors occurred during getting data. Please, reload page or wait some time.";
+            $(this).append(errorText);
+        }
+    });
 }
 
 function getEventsBySubcategoryId(subcategoryId) {
